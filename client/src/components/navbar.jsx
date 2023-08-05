d403dd27c3d80a36457c5e5f9c18af2a0ea2fbd8
scKamannavar <sagarck08@gmail.com>
2023-08-05 16:38:24 +0530

    Navbar basic

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -6,7 +6,7 @@ const navbar = () => {
     const [navdrop, setNavdrop] = useState(false);
   return (
     <div className=' bg-slate-400 sm:flex sm:justify-between sm:py-5'>
-        <div className='mb-2 h-20 sm:h-10 pt-5 sm:pt-0'>
+        <div className='mb-2 h-20 sm:h-8 pt-5 sm:pt-0'>
             <div className='flex items-center justify-between '>
                 <div className='px-4 '>
                     <img className='h-10 w-auto' src="https://upload.wikimedia.org/wikipedia/commons/5/52/Logo_de_Free.png" alt="" /> 
@@ -19,7 +19,7 @@ const navbar = () => {
                 </div>    
             </div>
         </div>
-        <div className = {navdrop ? 'px-4 pb-2 block' : 'hidden  sm:flex'} >
+        <div className = {navdrop ? 'px-4 pb-2 block ' : 'hidden  sm:flex pr-5'} >
             <div className='text-white hover:bg-slate-600 rounded px-2 py-1 '>Skills</div>
             <div className='text-white hover:bg-slate-600 rounded px-2 py-1 mt-1 sm:mt-0'>Projects</div>
             <div className='text-white hover:bg-slate-600 rounded px-2 py-1 mt-1 sm:mt-0'>Contact</div>
