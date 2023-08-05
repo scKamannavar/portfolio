d7cec81b7cfdc34de1e8158c15b7ffcfddc924a0
scKamannavar <sagarck08@gmail.com>
2023-08-05 16:22:36 +0530

    Setup and Navbar Basic

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState } from 'react'
+
+const navbar = () => {
+
+    const [navdrop, setNavdrop] = useState(false);
+  return (
+    <div className=' bg-slate-400 sm:flex sm:justify-between sm:py-5'>
+        <div className='mb-2 h-20 sm:h-10 pt-5 sm:pt-0'>
+            <div className='flex items-center justify-between '>
+                <div className='px-4 '>
+                    <img className='h-10 w-auto' src="https://upload.wikimedia.org/wikipedia/commons/5/52/Logo_de_Free.png" alt="" /> 
+                </div>
+                <div className='flex flex-col justify-centre px-3 pt-0 sm:hidden'>
+                    <button onClick={()=> setNavdrop(!navdrop)} type='button'>
+                            <img className={navdrop ? 'h-10 block'  : 'hidden'} src="https://www.svgrepo.com/show/485656/cancel2.svg" alt="" />            
+                            <img className={!navdrop ? 'h-10 block'  : 'hidden'} src="https://www.svgrepo.com/show/525355/hamburger-menu.svg" alt="" />     
+                    </button>  
+                </div>    
+            </div>
+        </div>
+        <div className = {navdrop ? 'px-4 pb-2 block' : 'hidden  sm:flex'} >
+            <div className='text-white hover:bg-slate-600 rounded px-2 py-1 '>Skills</div>
+            <div className='text-white hover:bg-slate-600 rounded px-2 py-1 mt-1 sm:mt-0'>Projects</div>
+            <div className='text-white hover:bg-slate-600 rounded px-2 py-1 mt-1 sm:mt-0'>Contact</div>
+            <div className='text-white hover:bg-slate-600 rounded px-2 py-1 mt-1 sm:mt-0'>Home</div>
+        </div>
+    </div>
+  )
+}
+
+export default navbar
