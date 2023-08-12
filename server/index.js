477d8e18419da8b2efede4c46b88fa6d4aa72bf7
scKamannavar <sagarck08@gmail.com>
2023-08-12 14:37:45 +0530

    Connection to Atlas, and Cors object

--- server/index.js
+++ server/index.js
@@ -5,11 +5,35 @@ const MessageModel = require('./model/message');
 // const UserModel = require('../client/src/Users.jsx');
 const app = express();
 
-app.use(cors())
+app.use(cors(
+    {
+        origin : [""],
+        method : ["POST", "GET"],
+        credentials : true
+    }
+))
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/Portfolio");
+// mongoose.connect("mongodb://127.0.0.1:27017/Portfolio");
 
+//Atlaas
+const dbUrl = "mongodb+srv://sagar080897:XK8BdC3GlIDreVuB@cluster0.8chijri.mongodb.net/Portfolio?retryWrites=true&w=majority";
+const connectionParams ={
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+}
+
+mongoose.connect(dbUrl,connectionParams)
+.then(()=>{
+    console.info('Mongo Atlas Connected')
+})
+.catch((err)=>{
+    console.log(err)
+})
+//Atlaas
+app.get("/",(req,res)=>{
+    res.json('Hello, Nothing else on this route... Just me ')
+})
 app.post("/sendMessage", (req, res)=>{
     MessageModel.create(req.body)
     .then(m => {
@@ -18,6 +42,7 @@ app.post("/sendMessage", (req, res)=>{
     })
     .catch(err => console.log(err))
 })
+
 
 app.listen(3000, () => {
     console.log("Server is running on 3000")
