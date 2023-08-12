1388745d624f58bb0d10267a9c9f66f557f046fa
SagarcK <36994488+scKamannavar@users.noreply.github.com>
2023-08-12 16:10:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin : [""],
+        origin : ["https://portfolio-sagarck.vercel.app"],
         method : ["POST", "GET"],
         credentials : true
     }
