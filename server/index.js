2c7a89f2565539bef279b5aad6234b4c2eae5647
scKamannavar <sagarck08@gmail.com>
2024-01-04 16:55:34 +0530

    app.use(cors())

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
   })
+  app.use(cors())
 
 
 // mongoose.connect("mongodb://127.0.0.1:27017/Portfolio");
