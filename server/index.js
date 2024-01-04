1502415240039c0c6b1b54e10f02991f2232d2f2
scKamannavar <sagarck08@gmail.com>
2024-01-04 14:36:09 +0530

    empty

--- server/index.js
+++ server/index.js
@@ -43,10 +43,10 @@ mongoose.connect(dbUrl,connectionParams)
     console.log(err)
 })
 //Atlaas
+// app.get("/",(req,res)=>{
+//     res.json('Hello, Nothing else on this route... Just me and my blogposts and some tacnhical posts yes')
+// })
 app.get("/",(req,res)=>{
-    res.json('Hello, Nothing else on this route... Just me and my blogposts and some tacnhical posts yes')
-})
-app.get("/random/",(req,res)=>{
     res.json('Hello, Randomness')
 })
 
